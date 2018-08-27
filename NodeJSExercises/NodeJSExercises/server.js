99e989622a1ee8deeab1f233eb54f1b5cfafcb58
Rob Sullivan <robertosullivan5@gmail.com>
2018-08-27 05:44:49 +0100

    added file system functionality

--- NodeJSExercises/NodeJSExercises/server.js
+++ NodeJSExercises/NodeJSExercises/server.js
@@ -1,8 +1,18 @@
 'use strict';
 var http = require('http');
+var fs = require('fs');
+
 var port = process.env.PORT || 1337;
+var data = fs.readFileSync("input.txt");
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.write(data);
+    res.end('\nHello World\n');
 }).listen(port);
+
+
+console.log("Server is at http://localhost:" + port + "/");
+
+console.log("\n" + data.toString());
+
